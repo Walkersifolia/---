cd18980f8088bf2c4d23b0ff7133bedb67ffa5f2
WalkerTian <903895182@qq.com>
2024-04-21 02:38:53 +0800

    自动请求管理员权限

--- 转圈圈.py
+++ 转圈圈.py
@@ -1,9 +1,19 @@
+import sys
 import ctypes
 import win32api, win32con
 import time
 
 def is_admin():
-    return ctypes.windll.shell32.IsUserAnAdmin()
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def request_admin():
+    if is_admin():
+        return
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 
 if is_admin():
     print("使用说明:\n\n按住鼠标第二侧键开转（这通常是靠近鼠标前端的侧键）\n\n作者：圈圈  B站：Walker_Tian")
@@ -13,5 +23,5 @@ if is_admin():
             win32api.mouse_event(win32con.MOUSEEVENTF_MOVE,800,0,0,0)
         time.sleep(0.01)
 else:
-    print("请以管理员模式运行此程序")
-    input("按Enter回车键退出程序...")
+    print("请点击“是”以管理员模式运行此程序")
+    request_admin()
